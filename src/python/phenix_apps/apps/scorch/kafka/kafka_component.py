b60332c257bdb997ee0fa2e3e35f08317f1feddc
Toohey, Hunter <hunter.toohey@nrel.gov>
2025-06-20 15:57:55 -0600

    add kafka
    add kafka component project script
    
    add debug output
    
    debug
    
    fix spacing issue
    
    capitalization
    
    simplifying to hopefully find bug
    
    debugging
    
    debug
    
    debug
    
    added quote
    
    add self.metadata
    
    tag error
    
    debug subscribe list
    
    add todo, import OS
    
    fix reference to functions within class
    
    add default topic, call self
    
    add comments and edit readme
    
    change default ip
    
    fix var names and yaml
    
    debug
    
    fix reference issue
    
    debug
    
    fix small bug
    
    debug
    
    debug messages
    
    debug messages
    
    better error output
    
    better error output
    
    better error output
    
    better error output
    
    fix open error
    
    remove while true, make global variable with stop stage, fix json error
    
    output data to phenix log (temporary)
    
    remove log message
    
    Add description in readme
    
    debugging, remove uneccesary debug comments, add more to replace them
    
    debug
    
    fix self reference in json
    
    tested the different filter settings to ensure they work:
    
    comments
    
    readme edit
    
    more clear readme
    
    more clear readme
    
    fx branch name
    
    fix branch name
    
    fix branch name
    
    fix branch name
    
    re-structure to be more applicable
    
    account for filter
    
    debug
    
    remove temp
    
    bug fix
    
    debuggin/ reworking, removed time conversion
    
    return to old script
    
    revert to earlier
    
    fix new script hopefully
    
    debugging
    
    fix json error
    
    fix that resolves repetitive message issue
    
    edit README
    
    remove debug outputs
    
    use global
    
    log change
    
    log change
    
    fix wildcards
    
    wildcards
    
    fix error in wildcard
    
    another attempt at wildward fix
    
    debug
    
    debug
    
    remove global
    
    re-add global
    
    epic debugging
    
    epic debugging
    
    fix wildcard bug to wait for topics to spawn if new experiment
    
    fix bug
    
    tested functionality, removed some debug comments
    
    rename boostrap serverse to kafka_ips
    
    remove uneccesary else clause
    
    add f.close() to finally clause
    
    rework and remove redundancy
    
    fix indentation error
    
    fix value logic for substations (maybe don't keep)
    
    debug
    
    remove wildcard test
    
    debug
    
    fix variable error
    
    subst fix
    
    no longer require filters
    
    revert back
    
    wildcards for keys
    
    wildcard value
    
    small bug
    
    fix regular expression
    
    multithreading attempt
    
    revert
    
    fix multi-threading
    
    more multithread debug
    
    variable name fix
    
    rework helper function for threading
    
    fix error where thread closes instantly
    
    fix path issue
    
    start issue
    
    try to fix consumer thread issue
    
    fix order of operations
    
    fix subscribed tags list issue
    
    self reference for consumer
    
    potential fix
    
    remove unecessary debug
    
    set daemon to true
    
    fix time issue
    
    remove daemon
    
    try to do writing in stop stage
    
    fix self issue
    
    fix typo, edit self ref
    
    debug
    
    change reference, move function
    
    edit call
    
    try again to just output the data at the end
    
    file error
    
    try mark ready to fix multithreading
    
    mark ready doesn't work, so try daemon with delayed output
    
    daemon false
    
    revert to older push
    
    fix revert
    
    fix revert
    
    changes from the comments/ suggestions
    
    fix Kafka typo
    
    fix global
    
    use self.eprint
    
    attempting to use nryan's pid/exectuable threading technique
    
    parallel processes
    
    remove debug logger, fix naming convention
    
    use self.eprint()
    
    remove unecessary sleep command
    
    change pid file name to be more descriptive, prevent issues if someone runs configure AND start
    
    attempt to use logger as output
    
    debug output
    
    debug logfile
    
    write to phenix log
    
    more debug output
    
    try logger
    
    remove debug output
    
    output topic names
    
    fix typo
    
    convert topic name to str
    
    get actual topic name
    
    subscribe to ALL topics if no topics selected
    
    default to all topics
    
    subscribe to all
    
    no filters means all data
    
    block position issue fix:

---
+++ src/python/phenix_apps/apps/scorch/kafka/kafka_component.py
@@ -0,0 +1,108 @@
+import subprocess
+import shlex
+import json
+import itertools
+import threading
+import time
+import sys
+import csv
+import re
+import os
+import time
+from phenix_apps.apps.scorch import ComponentBase
+from phenix_apps.common import logger, utils
+from kafka import KafkaConsumer
+from kafka.errors import KafkaError
+
+class Kafka(ComponentBase):
+    def __init__(self):
+        ComponentBase.__init__(self, 'kafka')
+        self.pid_file = f"/tmp/phenix-scorch-kafka-{self.exp_name}.pid"
+        self.execute_stage()
+        self.started = False
+        self.configured = False
+    
+    #configure just calls start
+    def configure(self):
+        self.configured = True
+        if not self.started:
+            start()
+
+    def start(self):
+        self.started = True
+        self.scorch_kafka_running = True
+        logger.log('INFO', f'Starting user component: {self.name}')
+
+        #get all variables from tags
+        kafka_ips = self.metadata.get("kafka_ips", ["172.20.0.63:9092"])
+        topics = self.metadata.get("topics", [])
+        csvBool = self.metadata.get("csv", True) #if false output a JSON
+
+        #get and output the output directory to the logger
+        output_dir = self.base_dir
+        logger.log('INFO', f'Output Directory: {output_dir}')
+        os.makedirs(output_dir, exist_ok=True)
+        if csvBool:
+            self.path = os.path.join(output_dir, f'{self.name}_output.csv')
+        else:
+            self.path = os.path.join(output_dir, f'{self.name}_output.json')
+
+        kafka_ips_str = ",".join(kafka_ips)
+        topics_str = json.dumps(topics)
+
+        #pass the inputs to the python file (which we execute as a separate process)
+        executable  = "/usr/local/lib/python3.12/dist-packages/phenix_apps/apps/scorch/kafka/kafka_listener.py"
+        arguments = f"python3 {executable} {csvBool} '{self.path}' {kafka_ips_str} '{topics_str}'"
+        command = shlex.split(arguments)
+
+        try:
+            log_file = open("/var/log/phenix/phenix.log", '+a')
+            response = subprocess.Popen(command, stdin=subprocess.DEVNULL, stdout=log_file, stderr=log_file, start_new_session=True)
+            self._create_pid_file(response.pid) #write PID to a file so that it can be found and killed later
+            response.poll() #prevents hang
+        except Exception as e:
+            self.eprint("ERROR", f"Error running listener executable. See: {e}")
+
+    def _create_pid_file(self, pid):
+        #writes PID to unique .pid file under /tmp directory
+        try:
+            with open(self.pid_file, "w+") as f:
+                f.write(str(pid))
+        except Exception as e:
+            self.eprint("ERROR", f"Could not create PID file. Listener process at PID {pid} will not terminate automatically when experiment ends. See: {e}")
+
+    def _consume_pid_file(self):
+        #reads and deletes PID file, returns PID
+        pid = 0
+        try: 
+            with open(self.pid_file, "r") as f:
+                pid = int(f.readline().rstrip())
+            os.remove(self.pid_file)
+            return pid
+        except Exception as e:
+            self.eprint("ERROR", f"Error consuming PID file. Listener process at PID {pid} will not be terminated. See: {e}")
+            return pid
+
+    def stop(self):
+        #stops listener executable
+        pid = self._consume_pid_file()
+
+        if not pid:
+            logger.log('INFO', f'Stopped user component: {self.name}')
+            exit()
+
+        try:
+            os.kill(pid, 9)
+            logger.log('INFO', f'Stopped user component: {self.name}')
+        except Exception as e:
+            self.eprint("ERROR", f"Error terminating listener at PID {pid}. See: {e}")
+
+    #cleanup is the same as stop
+    def cleanup(self):
+        self.stop()
+
+def main():
+    Kafka()
+
+if __name__ == '__main__':
+    main()
