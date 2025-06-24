9d7f7cdee263e31e8a0232e536ada5c8f5bd3e87
Toohey, Hunter <hunter.toohey@nrel.gov>
2025-06-24 08:47:19 -0600

    debugging

--- src/python/phenix_apps/apps/scorch/kafka/kafka.py
+++ src/python/phenix_apps/apps/scorch/kafka/kafka.py
@@ -1,29 +1,110 @@
-import json, itertools, threading, time, sys, csv, re
+
+import subprocess, sys
 
 from phenix_apps.apps.scorch import ComponentBase
 from phenix_apps.common import logger, utils
-from datetime import datetime
 
-class kafka(ComponentBase):
+# TODO: merge tcpdump's functionality into the 'pcap' component
+
+class TCPDump(ComponentBase):
     def __init__(self):
-        ComponentBase.__init__(self, 'kafka')
+        ComponentBase.__init__(self, 'tcpdump')
+
         self.execute_stage()
+
 
     def start(self):
         logger.log('INFO', f'Starting user component: {self.name}')
 
-        logger.log('INFO', f'Configured user component: {self.name}')
+        vms = self.metadata.get('vms', None)
+
+        mm = self.mm_init()
+
+        for vm in vms:
+            hostname = vm.get('hostname', None)
+            iface    = vm.get('iface',    None)
+            options  = vm.get('options',  '')
+            filter   = vm.get('filter',   '')
+
+            if not hostname:
+                self.eprint('no hostname provided for VM config')
+                sys.exit(1)
+
+            if not iface:
+                self.eprint('no interface name provided for VM config')
+                sys.exit(1)
+
+            res = utils.mm_exec_wait(mm, hostname, 'which tcpdump')
+            if not res['stdout']:
+                self.eprint(f'tcpdump is not installed in VM {hostname}')
+                sys.exit(1)
+
+            self.print(f'starting tcpdump on interface {iface} in VM {hostname}')
+
+            if filter:
+                self.print(f'using filter {filter} for tcpdump in VM {hostname}')
+
+            mm.cc_filter(f'name={hostname}')
+            mm.cc_exec(f'ip link set {iface} up')
+            mm.cc_background(f'tcpdump {options} -i {iface} -U -w /dump-{iface}.pcap {filter}')
+
+        logger.log('INFO', f'Started user component: {self.name}')
+
 
     def stop(self):
-        #it should run as long as the experiment runs, so I don't think anything needs to be here
         logger.log('INFO', f'Stopping user component: {self.name}')
 
-    def cleanup(self):
-        #no cleanup, currently it just makes and populates the one csv file
-        logger.log('INFO', f'Cleaning up user component: {self.name}')
+        vms = self.metadata.get('vms', None)
+
+        mm = self.mm_init()
+
+        convert = self.metadata.get('convertToJSON', False)
+
+        if convert:
+            self.print(f'PCAP --> JSON conversion enabled... please be patient')
+        else:
+            self.print(f'PCAP --> JSON conversion disabled')
+
+        for vm in vms:
+            hostname = vm.get('hostname', None)
+            iface    = vm.get('iface',    None)
+
+            if not hostname:
+                self.eprint('no hostname provided for VM config')
+                sys.exit(1)
+
+            if not iface:
+                self.eprint('no interface name provided for VM config')
+                sys.exit(1)
+
+            pcap_out = f'{self.base_dir}/{hostname}-{iface}.pcap'
+            json_out = f'{self.base_dir}/{hostname}-{iface}.pcap.jsonl'
+
+            utils.mm_exec_wait(mm, hostname, 'pkill tcpdump')
+
+            self.print(f'copying PCAP file from node {hostname}...')
+
+            utils.mm_recv(mm, hostname, f'/dump-{iface}.pcap', pcap_out)
+            mm.cc_exec(f'rm /dump-{iface}.pcap')
+
+            self.print(f'done copying PCAP file from node {hostname}')
+
+            if convert:
+                self.print(f'starting PCAP --> JSON conversion for node {hostname}...')
+
+                subprocess.run(
+                    f"bash -c 'tshark -r {pcap_out} -T ek > {json_out} 2>/dev/null'",
+                    shell=True,
+                )
+
+                self.print(f'PCAP --> JSON conversion for node {hostname} complete')
+
+        logger.log('INFO', f'Stopped user component: {self.name}')
+
 
 def main():
-    kafka()
-    
+    TCPDump()
+
+
 if __name__ == '__main__':
     main()
