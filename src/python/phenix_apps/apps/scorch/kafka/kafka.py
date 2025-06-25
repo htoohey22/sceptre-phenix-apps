fbaf92426dd1a90891a297e98e9eb42fc89adf89
Toohey, Hunter <hunter.toohey@nrel.gov>
2025-06-25 15:18:01 -0600

    fix branch name

--- src/python/phenix_apps/apps/scorch/kafka/kafka.py
+++ src/python/phenix_apps/apps/scorch/kafka/kafka.py
@@ -45,7 +45,7 @@ class kafka(ComponentBase):
         substation =  self.metadata.get("substation", "")
         csvOut = self.metadata.get("csv", True)
 
-        #logger.log('INFO', f'branch name: {{BRANCH_NAME}}')
+        logger.log('INFO', f'branch name: {{{BRANCH_NAME}}}')
 
         '''
         logger.log('INFO', f'bootstrapServers: {bootstrapServers}')
