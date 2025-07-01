075f13f9102eeddc521c25b7dc2c6663999f6ec9
Toohey, Hunter <hunter.toohey@nrel.gov>
2025-07-01 14:41:10 -0600

    fix order of operations

--- src/python/phenix_apps/apps/scorch/kafka/kafka.py
+++ src/python/phenix_apps/apps/scorch/kafka/kafka.py
@@ -31,6 +31,37 @@ class kafka(ComponentBase):
         all_keys = set()
         wrote_header = False
 
+        #list of all topic names we want the consumer to subscribe to
+        subscribedTopics = []
+        foundTopics = False
+
+        #get all topic names
+        if not topics:
+            logger.log('INFO', f'No topics subscribed to')
+        
+        for topic in topics:
+            name =  topic.get("name")
+
+            #handle wildcards in the name, this only supports right wildcards
+            if '*' in name:
+                foundTopics = False
+                filteredName = name.split('*')[0] #we don't care about anything right of the wildcard
+                pattern = f'^{re.escape(filteredName)}.*'
+                while foundTopics == False: #if this is a new experiment, kafka may not have populated any tags... so wait until it has
+                    for topic in consumer.topics():
+                        if str(filteredName) in str(topic):
+                            subscribedTopics.append(topic)
+                    if subscribedTopics:
+                        foundTopics = True
+                    else:
+                        time.sleep(5) #we don't need to be constantly scaning for data, so sleep for a few seconds imbetween attempts
+            elif name:
+                subscribedTopics.append(name)
+
+        logger.log('INFO', f'Subscribed Topics: {subscribedTopics}')
+        #subscribe to all topic names
+        consumer.subscribe(subscribedTopics)
+
         def helper(csvBool, path, kafka_ips, subscribedTopics, topics):
             try:
 
@@ -42,37 +73,6 @@ class kafka(ComponentBase):
                     enable_auto_commit=False,
                     value_deserializer=lambda m: json.loads(m.decode('utf-8'))
                 )
-
-                #list of all topic names we want the consumer to subscribe to
-                subscribedTopics = []
-                foundTopics = False
-
-                #get all topic names
-                if not topics:
-                    logger.log('INFO', f'No topics subscribed to')
-                
-                for topic in topics:
-                    name =  topic.get("name")
-
-                    #handle wildcards in the name, this only supports right wildcards
-                    if '*' in name:
-                        foundTopics = False
-                        filteredName = name.split('*')[0] #we don't care about anything right of the wildcard
-                        pattern = f'^{re.escape(filteredName)}.*'
-                        while foundTopics == False: #if this is a new experiment, kafka may not have populated any tags... so wait until it has
-                            for topic in consumer.topics():
-                                if str(filteredName) in str(topic):
-                                    subscribedTopics.append(topic)
-                            if subscribedTopics:
-                                foundTopics = True
-                            else:
-                                time.sleep(5) #we don't need to be constantly scaning for data, so sleep for a few seconds imbetween attempts
-                    elif name:
-                        subscribedTopics.append(name)
-
-                logger.log('INFO', f'Subscribed Topics: {subscribedTopics}')
-                #subscribe to all topic names
-                consumer.subscribe(subscribedTopics)
 
                 with open(path, 'a', newline='', encoding='utf-8') as file:
                     writer = None
