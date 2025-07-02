600621cb84a3c0c93cca09cfb8a31753b5730cc5
Toohey, Hunter <hunter.toohey@nrel.gov>
2025-07-02 10:45:00 -0600

    time.sleep

--- src/python/phenix_apps/apps/scorch/kafka/kafka.py
+++ src/python/phenix_apps/apps/scorch/kafka/kafka.py
@@ -42,7 +42,7 @@ class Kafka(ComponentBase):
         else:
             self.path = os.path.join(output_dir, f'kafka_{self.name}_output.json')
 
-        sleep(2)
+        time.sleep(2)
 
 
         kafka_ips_str = ",".join(kafka_ips)
