7727ff46dd15c98353ff8039aad5177fc6a6015b
Toohey, Hunter <hunter.toohey@nrel.gov>
2025-09-16 12:45:43 -0600

    use os getenv

--- src/python/phenix_apps/apps/scorch/kafka/kafka_component.py
+++ src/python/phenix_apps/apps/scorch/kafka/kafka_component.py
@@ -18,8 +18,8 @@ from kafka import KafkaConsumer
 from kafka.errors import KafkaError
 from pathlib import Path
 
-#PHENIX_LOG = "/var/log/phenix/phenix.log"
-PHENIX_LOG = os.getenv("PHENIX_LOG_FILE")
+PHENIX_LOG = "/var/log/phenix/phenix.log"
+#PHENIX_LOG = os.getenv("PHENIX_LOG_FILE")
 #PHENIX_LOG = settings.PHENIX_LOG_FILE
 
 class Kafka(ComponentBase):
@@ -83,7 +83,7 @@ class Kafka(ComponentBase):
             #global PHENIX_LOG
             log_file = open(PHENIX_LOG, '+a')
 
-            response = subprocess.Popen(command, stdin=subprocess.DEVNULL, stdout=None, stderr=None, start_new_session=True)
+            response = subprocess.Popen(command, stdin=subprocess.DEVNULL, stdout=log_file, stderr=log_file, start_new_session=True)
             self._create_pid_file(response.pid) #write PID to a file so that it can be found and killed later
             response.poll() #prevents hang
         except Exception as e:
