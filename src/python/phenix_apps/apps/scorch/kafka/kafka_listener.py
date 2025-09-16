fe4f13e4d79cff730e338c423f4b19649f74117e
Toohey, Hunter <hunter.toohey@nrel.gov>
2025-09-16 13:47:32 -0600

    print log to scorch component

--- src/python/phenix_apps/apps/scorch/kafka/kafka_listener.py
+++ src/python/phenix_apps/apps/scorch/kafka/kafka_listener.py
@@ -30,17 +30,17 @@ def run(csvBool, path, kafka_ips, topics):
         value_deserializer=lambda m: json.loads(m.decode('utf-8'))
     )
 
-    #list of all topic names we want the consumer to subscribe to
-    subscribedTopics = []
-    foundTopics = False
-
     #get all topic names
     if not topics:
         consumer.subscribe(pattern=".*")
         
     else:
         start = time.time()
-        
+
+        #list of all topic names we want the consumer to subscribe to
+        subscribedTopics = []
+        foundTopics = False
+
         for topic in topics:
             name =  topic.get("name")
         
